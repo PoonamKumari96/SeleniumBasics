ee3034e92741479b10dd2eace08e5b5df924b6d7
PoonamKumari96 <erpunu96@gmail.com>
2023-08-29 16:40:47 +0530

    added mouseHover test

---
+++ src/main/java/HerokuAppTesting/keyPressTest.java
@@ -0,0 +1,28 @@
+package HerokuAppTesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.WebElement;
+
+public class keyPressTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "Z:\\Projects\\SeleniumBasics\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//a[normalize-space()='Key Presses']")).click();
+        WebElement typeText = driver.findElement(By.id("target"));
+        typeText.sendKeys(" ");
+        System.out.println(driver.findElement(By.xpath("//p[@id='result']")).getText());
+
+        Thread.sleep(3000);
+        driver.quit();
+
+
+
+    }
+}
