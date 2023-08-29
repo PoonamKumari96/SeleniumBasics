9abd124ed68959e9387c15cfefe36c415d812cc3
PoonamKumari96 <erpunu96@gmail.com>
2023-08-29 16:49:39 +0530

    added KeyPressTest continue

--- src/main/java/HerokuAppTesting/KeyPressTest.java
+++ src/main/java/HerokuAppTesting/KeyPressTest.java
@@ -1,2 +1,24 @@
-package HerokuAppTesting;public class KeyPressTest {
+package HerokuAppTesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class KeyPressTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "Z:\\Projects\\SeleniumBasics\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//a[normalize-space()='Key Presses']")).click();
+        WebElement typeText = driver.findElement(By.id("target"));
+        typeText.sendKeys(" ");
+        System.out.println(driver.findElement(By.xpath("//p[@id='result']")).getText());
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
 }
