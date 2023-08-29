297539ce90dfa99b59583c1f3fa1732b8e4b1ed7
PoonamKumari96 <erpunu96@gmail.com>
2023-08-29 16:49:09 +0530

    added KeyPressTest

---
+++ src/main/java/HerokuAppTesting/KeyPressTest.java
@@ -0,0 +1,2 @@
+package HerokuAppTesting;public class KeyPressTest {
+}
--- src/main/java/HerokuAppTesting/keyPressTest.java
+++ src/main/java/HerokuAppTesting/keyPressTest.java
@@ -2,11 +2,10 @@ package HerokuAppTesting;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class keyPressTest {
+public class KeyPressTest {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "Z:\\Projects\\SeleniumBasics\\chromedriver.exe");
 
@@ -21,8 +20,5 @@ public class keyPressTest {
 
         Thread.sleep(3000);
         driver.quit();
-
-
-
     }
 }
