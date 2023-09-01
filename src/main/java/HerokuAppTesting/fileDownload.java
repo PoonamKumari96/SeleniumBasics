9e3fbcc0c41cf750d9fc6237617b2ec3f92213f6
PoonamKumari96 <erpunu96@gmail.com>
2023-09-01 16:35:02 +0530

    added fileDownload on user location

--- src/main/java/HerokuAppTesting/fileDownload.java
+++ src/main/java/HerokuAppTesting/fileDownload.java
@@ -3,12 +3,26 @@ package HerokuAppTesting;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.util.HashMap;
 
 public class fileDownload {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "Z:\\Projects\\SeleniumBasics\\chromedriver.exe");
 
-        WebDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+
+        String downloadFilePath = "Z:\\projects\\SeleniumBasics";
+        chromePrefs.put("download.default_directory", downloadFilePath);
+
+        options.setExperimentalOption("prefs", chromePrefs);
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://the-internet.herokuapp.com/");
         Thread.sleep(2000);
         driver.manage().window().maximize();
@@ -17,8 +31,17 @@ public class fileDownload {
         Thread.sleep(2000);
 
         driver.findElement(By.linkText("icon.png")).click();
+        Thread.sleep(5000);
+
+        File file = new File(downloadFilePath + "\\icon.png");
+        if(file.exists()){
+            System.out.println("file got successfully downloaded");
+        }
+        else {
+            System.out.println("file not found");
+        }
         Thread.sleep(3000);
-        driver.quit();
+        //driver.quit();
 
 
 
